25809d0fca296bf980b7d0e68df4b9c6d6b3116a
Nathaniel <nathanielbughar@outlook.com>
2022-09-04 17:08:13 +0100

    back end update

--- api/routes.js
+++ api/routes.js
@@ -3,65 +3,33 @@ const router = new Router();
 
 const lrProperty = require('./models/lrProperty.js');
 
-// router.param('lrPropertyId', async (id, ctx, next) =>
-// {
-// 	ctx.lrProperty = await new lrProperty({id: id}).fetch({withRelated: ['lrTransactions'], require: false});
-
-// 	if(! ctx.lrProperty)
-// 	{
-// 		ctx.status = 404;
-// 		return ctx.body = {error: true, msg: "LRProperty not found"};
-// 	}
-
-// 	return next();
-
-// })
-// .get('/', async (ctx, next) => 
-// {
-// 	return ctx.body = "I'm alive again!";
-// })
-// .get('/show', async (ctx, next) => 
-// {
-// 	return ctx.body = "Hello world";
-// })
-// .get('/lrProperty/:lrPropertyId', async (ctx, next) => 
-// {
-// 	return ctx.body = {success: true, lrProperty: ctx.lrProperty.toJSON()};
-// })
-
-
-router.param('search', async (search_string, ctx, next) =>
-{
-	ctx.lrProperty = await lrProperty.query(function (lr) { 
-		lr.where('outcode', 'LIKE', `%${search_string}%`)
-		.orWhere('incode', 'LIKE', `%${search_string}%`)
-		.orWhere('street', 'LIKE', `%${search_string}%`)
-		.orWhere('id', 'LIKE', `%${search_string}%`)
+router.param('search', async (search_string, ctx, next) => {
+	ctx.lrProperty = await lrProperty.query((lr)=> {
+		lr.where('outcode', 'LIKE', search_string.substring(0, 3))
+			.orWhere('incode', 'LIKE', search_string.substring(3, 6))
+			.orWhere('street', 'LIKE', `%${search_string}%`)
+			.orWhere('id', 'LIKE', `%${search_string}%`)
 	})
+		.fetch({ withRelated: ['lrTransactions'], require: false });
 
-	.fetch({withRelated: ['lrTransactions'], require: false});
-
-	if(! ctx.lrProperty)
-	{
+	if (!ctx.lrProperty) {
 		ctx.status = 404;
-		return ctx.body = {error: true, msg: "LRProperty not found"};
+		return ctx.body = { error: true, msg: "LRProperty not found" };
 	}
 
 	return next();
 
 })
-.get('/lrProperty/:search', async (ctx, next) => 
-{
-	return ctx.body = {success: true, lrProperty: ctx.lrProperty};
-})
+	.get('/lrProperty/:search', async (ctx, next) => {
+		return ctx.body = { success: true, lrProperty: ctx.lrProperty };
+	})
+	.get('/', async (ctx, next) => {
+		return ctx.body = "Server started!";
+	})
 
 
-
-
-
-module.exports = (app) =>
-{
+module.exports = (app) => {
 	app
-	.use(router.routes())
-	.use(router.allowedMethods());
+		.use(router.routes())
+		.use(router.allowedMethods());
 };
