4e853e4ea3f16dfe9f13a2cd03e5b812f28577e1
Nathaniel <nathanielbughar@outlook.com>
2022-09-04 17:03:38 +0100

    Front end fixes: Added react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,69 @@
-import {useState, useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 
-function App()
-{
-
-  /* This is example of how to fetch data from API */
+function App() {
   const [propertyData, setPropertyData] = useState(null);
   const [searchString, setSearchString] = useState()
-  const [searchData, setSearchData] = useState()
 
-  const acceptSearch = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
     const resp = await fetch(`/lrProperty/${searchString}`);
     const json = await resp.json();
-
-    if(json.success)
-      setSearchData(json.lrProperty)
-
-      console.log('Got here', searchData?.street);
+    if (json.success) setPropertyData(json.lrProperty);
   }
-
-  useEffect(() =>
-  {
-    async function fetchData()
-    {
-      // demo request to API (ensure it is running!)
-      const resp = await fetch("/lrProperty");
-      const json = await resp.json();
-
-      if(json.success)
-        setPropertyData(json.lrProperty)
-    }
-    
-    fetchData();
-
-    console.log('property data', propertyData)
-  }, []);
-  /* end example */
-
-
 
   return (
     <div className="App">
-      <header className="App-header">
-        <form onSubmit={acceptSearch}>
-          <input type={'text'} required onChange={(e)=>setSearchString(e.target.value)} />
-          <button type='submit'>Submit</button>
-        </form>
-        {propertyData ? <strong>{propertyData.street}</strong> : null}
-        {searchData ? <strong>{searchData.street}</strong> : null}
-      </header>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <Row className="align-items-center">
+            <Col sm={10}>
+              <Form.Control placeholder="Search for property." onChange={(e) => setSearchString(e.target.value)} />
+            </Col>
+            <Col sm={2}>
+              <Button type="submit">Submit!</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+      <br />
+      <Container>
+        {propertyData && 
+        <strong>{propertyData.paon}, {propertyData.street}, {propertyData.outcode.toString() + " " + propertyData.incode.toString()} </strong>}
+      </Container>
+      <br />
+      <Container>
+        {propertyData &&
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Property ID</th>
+                <th>Date</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {propertyData && propertyData.lrTransactions.map((tx) => (
+                <tr key={tx.id}>
+                  <td>{tx.id}</td>
+                  <td>{tx.date}</td>
+                  <td>{tx.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        }
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+
